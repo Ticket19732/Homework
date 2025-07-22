2e95bec4b4732597b27473cbb118eea8c5487d2f
Ticket19732 <80239096+Ticket19732@users.noreply.github.com>
2025-07-22 14:44:15 +0800

    Update recursion1.cpp

--- homework1/src/main.cpp
+++ homework1/src/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int sigma(int n) {
-    if (n < 0)
-        throw "n < 0";
-    else if (n <= 1)
-        return n;
-    return n + sigma(n - 1);
+int ackermann_recursive(int m, int n) {
+    if (m == 0) return n + 1;
+    else if (n == 0) return ackermann_recursive(m - 1, 1);
+    else return ackermann_recursive(m - 1, ackermann_recursive(m, n - 1));
 }
 
 int main() {
-    int result = sigma(3);
-    cout << result << '\n';
+    int m, n;
+    cout << "Enter m and n: ";
+    cin >> m >> n;
+    cout << "Ackermann(" << m << ", " << n << ") = " << ackermann_recursive(m, n) << endl;
+    return 0;
 }
